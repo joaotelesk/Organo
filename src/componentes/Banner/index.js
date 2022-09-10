819c70f9fdde874a4a6e8424fa3897cff6a545a1
Joao Teles <joaojoseteles@gmail.com>
2022-09-09 23:37:28 -0300

    Projeto Organo - alura

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.svg"
+        alt="O banner principal da página do Organo"
+      ></img>
+    </header>
+  );
+};
+export default Banner;
