0ac24587b74f3bbbd7d6ec15f7ff92e7d35ca9f0
Joao Teles <joaojoseteles@gmail.com>
2022-11-21 17:51:34 -0300

    :recycle: refactor: componet times passado para tsx

---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,37 @@
+import { Icorabodaor } from "../../compartinhado/interfaces/IColaborador";
+import Colaborador from "../Colaborador";
+import "./Time.css";
+
+interface TimeProps {
+  corPrimaria: string;
+  corSecundaria: string;
+  nome: string;
+  colaboradores: Icorabodaor[];
+}
+const Time = ({
+  colaboradores,
+  corPrimaria,
+  corSecundaria,
+  nome,
+}: TimeProps) => {
+  return colaboradores.length > 0 ? (
+    <section className="time" style={{ backgroundColor: corSecundaria }}>
+      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+      <div className="colaboradores">
+        {colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corDeFundo={corPrimaria}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
+
+export default Time;
